46ba82635cb70ce8ef7f13b03e825de9cf61c6de
ElyDev <hadewunmi026@gmail.com>
2023-04-16 12:35:07 +0100

    first commit

--- src/middleware/login.middlewarw.ts
+++ src/middleware/login.middlewarw.ts
@@ -4,7 +4,16 @@ import { userloginSchema } from '../schema/user.schema'
 import { userLogin } from '../controllers/auth.controller'
 import User from '../models/user.model'
 import { currentUser } from '../validator/usersvalidator'
+import { AnyARecord } from 'dns'
 
+// async function loginHandler(req: Request, res: Response) {
+//     try{
+//         const {email, password}= await userloginSchema.validateAsync(req.body)
+
+//     } catch(error: any){
+//         res.status(400).json({error: error.message})
+//     }
+// }
 
 const loginHandler = (userSchema: joi.Schema) => (req: Request, res: Response, next: NextFunction) => {
 
@@ -15,9 +24,8 @@ const loginHandler = (userSchema: joi.Schema) => (req: Request, res: Response, n
         // throw new Error
        return res.status(400).send(error.details[0].message)
 
-    } 
-    next()
+    
+    }
 }
-
 
 export default loginHandler
