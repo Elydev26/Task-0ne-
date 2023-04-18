a55f856764fc84f1d606f1ccfb240af60bf1002c
ElyDev <hadewunmi026@gmail.com>
2023-04-18 08:57:44 +0100

    task

--- src/middleware/login.middlewarw.ts
+++ src/middleware/login.middlewarw.ts
@@ -1,22 +1,19 @@
 import { Request, Response, NextFunction } from 'express'
-import joi, { string } from 'joi'
-import { userloginSchema } from '../schema/user.schema'
-import { userLogin } from '../controllers/auth.controller'
-import User from '../models/user.model'
-import { currentUser } from '../service/user.service'
-import { AnyARecord } from 'dns'
+import joi from 'joi'
 
 
-const loginHandler = (userSchema: joi.Schema) => (req: Request, res: Response, next: NextFunction) => {
 
-    const { error } = userSchema.validate(req)
-    console.log("these is the response----->", error?.message)
+const loginHandler = (userloginSchema: joi.Schema) => (req: Request, res: Response, next: NextFunction) => {
+
+    const { error } = userloginSchema.validate(req)
+     console.log("these is the response----->", error?.message)
     if (error) {
 
        return res.status(400).send(error.details[0].message)
 
     
     }
+    next()
 }
 
 export default loginHandler
